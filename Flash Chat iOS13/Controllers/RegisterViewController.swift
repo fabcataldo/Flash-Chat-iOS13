a32eeb30910beb35cc0f370db78053695661b68c
Fabio <fabio@MacBook-Air-de-Fabio.local>
2021-11-01 23:48:13 -0300

    Added CLTypinglabel pod. Added basic management of login, register and logout features.

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    @IBOutlet weak var ErrorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        ErrorLabel.text = ""
+    }
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    self.ErrorLabel.text = e.localizedDescription
+                    print(e.localizedDescription)
+                } else{
+                    //navigate to the chatviewcontroller
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
