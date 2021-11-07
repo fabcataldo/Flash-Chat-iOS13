690028c157acfd372f423bd4829cff85a11600e7
Fabio <fabio@MacBook-Air-de-Fabio.local>
2021-11-07 17:52:26 -0300

    Added basic functionalities about sending and retrievering messages. Added Firestore setup. Added stuff related to UI components.

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -13,14 +13,61 @@ class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    let db = Firestore.firestore()
+    
+    var messages:[Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "⚡️FlashChat"
+//        tableView.delegate = self
+        title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        loadMessages()
+    }
+    
+    func loadMessages(){
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+            self.messages = []
+            
+            if let e = error{
+                print("There was an issue retrieving data from Firestore. \(e)")
+            } else{
+                if let snapshotDocument = querySnapshot?.documents{
+                    for doc in snapshotDocument{
+                    
+                        let data = doc.data()
+                        if let sender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String{
+                            let newMessage = Message(sender: sender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
+                if let e = error{
+                    print("There was an issue saving data to Firestore, \(e)")
+                } else{
+                    print("Successfully saved data.")
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -31,5 +78,27 @@ class ChatViewController: UIViewController {
             print("Error signing out: %@", signOutError)
         }
     }
-    
 }
+
+// el protocol UITableViewDataSource es el encargado de manejar al TableView en el sentido de decir cuantas filas se necesitan (1er metodo)
+// y de donde sacar los datos (2do metodo)
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        se crea una nueva celda con el metodo dequeueReusableCell() con argumentos: el primero, es el identificador que le puse a la celda en el MessageCell.xib (ver atributos, identifier del elemento), y con el casteo del as! casteo la nueva celda a la celda customizada mia: MessageCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
+// si quiero habilitar esto, no olvidar habilitarlo en el STORYBOARD, en el ReusableCell, en el atributo Selection con el valor que quiera, por ej "grey"
+//extension ChatViewController: UITableViewDelegate{
+////  la funcion de abajo se llama cuando se selecciona una fila del TableView
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Fabio on 05/11/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+struct Message{
+    let sender :String
+    let body: String
+}
